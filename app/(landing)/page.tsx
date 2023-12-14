51d9b801b33441102ef28af118021d2b47c2b0d7
Saif Aljashamy <supersaifaljashamy@gmail.com>
2023-12-14 15:17:10 -0800

    Remove import not needed

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,6 +1,5 @@
 import { LandingNavbar } from "@/components/landing-navbar";
 import { LandingHero } from "@/components/landing-hero";
-import { LandingContent } from "@/components/landing-content";
 
 const LandingPage = () => {
   return ( 
